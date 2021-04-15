a658435c10016b4e79d29d67c87c10eb1da47c02
Freddie Sackur <github@dustyfox.uk>
2021-04-15 21:05:58 +0100

    Find-GithubCode finds code on Github

--- Public/Find-GithubCode.ps1
+++ Public/Find-GithubCode.ps1
@@ -4,7 +4,7 @@ function Find-GithubCode
     [CmdletBinding(PositionalBinding = $false)]
     param
     (
-        [Parameter(Mandatory, ValueFromPipeline, ValueFromRemainingArguments)]  #Position = 0,
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromRemainingArguments)]
         [string[]]$Keyword,
 
         [Parameter()]
@@ -21,6 +21,7 @@ function Find-GithubCode
 
         [string]$Language,
 
+        [Size()]
         [string]$Size
     )
 
@@ -50,45 +51,26 @@ function Find-GithubCode
         # throw
     }
 
-    $SearchTerms = [ordered]@{}
+
     $Params = $MyInvocation.MyCommand.Parameters.Values | Where-Object {$_.Name -notin $COMMON_PARAMETERS}
+
+    $SearchTerms = [Collections.Generic.List[string]]::new()
     foreach ($Param in $Params)
     {
-        $Name      = $Param.Name
-        $Value     = Get-Variable $Name -ValueOnly -ErrorAction SilentlyContinue
-        if (-not $Value)
+        $Name  = $Param.Name
+        $Value = Get-Variable $Name -ValueOnly -ErrorAction SilentlyContinue
+
+        if ($Name -eq 'Keyword')
         {
-            continue
+            $SearchTerms.AddRange($Value)
         }
-
-        $Param.Attributes.ValidatePattern
-        # if ($Param.Attributes.ValidatePattern)
-        # $Qualifier = $Name.ToLower()
-        # $Qualifier
+        elseif ($Value)
+        {
+            $Term = "{0}:{1}" -f $Name.ToLower(), $Value
+            $SearchTerms.Add($Term)
+        }
     }
+
+    $Uri = "/search/code?q=$($SearchTerms -join '+')"
+    Invoke-GithubMethod $Uri
 }
-<#
-in:file	octocat in:file matches code where "octocat" appears in the file contents.
-in:path	octocat in:path matches code where "octocat" appears in the file path.
-
-user:USERNAME	user:defunkt extension:rb matches code from @defunkt that ends in .rb.
-org:ORGNAME	org:github extension:js matches code from GitHub that ends in .js.
-repo:USERNAME/REPOSITORY	repo:mozilla/shumway extension:as matches code from @mozilla's shumway project that ends in .as.
-
-path:/	octocat filename:readme path:/ matches readme files with the word "octocat" that are located at the root level of a repository.
-path:DIRECTORY	form path:cgi-bin language:perl matches Perl files with the word "form" in a cgi-bin directory, or in any of its subdirectories.
-path:PATH/TO/DIRECTORY	console path:app/public language:javascript matches JavaScript files with the word "console" in an app/public directory, or in any of its subdirectories (even if they reside in app/public/js/form-validators).
-
-language:LANGUAGE
-
-
-size:n	function size:>10000 language:python matches code with the word "function," written in Python, in files that are larger than 10 KB.
-    greater than, less than, and range qualifiers
-
-filename:FILENAME	filename:linguist matches files named "linguist."
-filename:.vimrc commands matches .vimrc files with the word "commands."
-filename:test_helper path:test language:ruby matches Ruby files named test_helper within the test directory.
-
-extension:EXTENSION	form path:cgi-bin extension:pm matches code with the word "form," under cgi-bin, with the .pm file extension.
-icon size:>200000 extension:css matches files larger than 200 KB that end in .css and have the word "icon."
-#>
